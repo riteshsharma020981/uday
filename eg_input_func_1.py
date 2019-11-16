71d3e2a911bbcd2002a5a9f6d9fd497198d81a26
riteshsharma020981 <57810016+riteshsharma020981@users.noreply.github.com>
2019-11-16 12:36:16 +0530

    Add files via upload

---
+++ eg_input_func_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# input() function simple examples
+
+# Print instruction to type
+print('Type your email string in the terminal : ' + '\n')
+# storing email input in a variable email
+email = input()
+# Printing email variable
+print('My Email is :' + email)
+# check & Print data type of the variable email
+print(type(email))
+# Print instruction to type
+print('Type your url in the terminal : ' + '\n')
+# storing url input in a variable email
+url = input()
+# Printing url variable
+print('My url is :' + url)
---
+++ eg_input_func_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# print function example / used case
+
+
+# input function example
+def input_func():
+    email = input()
+    return email
+
+
+# call above function and print the return value
+print('The value of email function is :' + input_func())
